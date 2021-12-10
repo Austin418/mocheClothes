55bdd8ff4c85366f8ee47b825ebbded1c11a1411
Jason Enciso <Jencis275@west-mec.local>
2021-12-10 15:20:15 -0700

    updated some files

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("express-async-errors");
 
 const express = require("express");
 const app = express();
+const connectDB = require('./db/connect')
 
 const notFoundError = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -11,7 +12,7 @@ const stripeController = require("./controllers/stripeController");
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(fileUpload({ useTempFiles: true }));
+// app.use(fileUpload({ useTempFiles: true }));
 app.use(express.static("./public"));
 app.get("/", (req, res) => {
   res.send("<h1>Stripe payment Starter</h1>");
--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPW_KEY)
 
@@ -16,24 +15,5 @@ const stripeController = async (req, res) => {
   })
   res.json({clientSecret: paymentIntent.client_secret})
 }
-=======
-require("dotenv").config();
-const stripeController = require('stripe')(process.env.STRIPE_KEY)
-
-const stripeController = async (req, res) => {
-    const {purchase, total_amount, shipping_fee} = req.body
-}
-
-const calcTotal = () => {
-    return total_amount + shipping_fee
-}
-
-const paymentIntent = await stripe.paymentIntent.create({
-    amount: calcTotal(),
-    currency: "usd",
-})
-
-res.json({clientSecret: paymentIntent.client_secret})
->>>>>>> 101fc0562519e7163524af73b5554cca88337ba0
 
 module.exports = stripeController
