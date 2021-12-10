84c9bf401ec70ca87439b6464111fb53b3f30d01
Austin Farwell <Afarwe182@west-mec.local>
2021-12-10 13:58:15 -0700

    merge

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ require('express-async-errors')
 
 const express = require('express')
 const app = express()
-const fileUpload = require('express-fileupload')
 
-const connectDB = require('./db/connect')
-const productRouter = require('./routes/productsRouter')
 
 const notFoundError = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
-
+const stripeController = require('./controllers/stripeController')
 
 const port = process.env.PORT || 3000
 
@@ -18,9 +15,9 @@ app.use(express.json())
 app.use(fileUpload({useTempFiles: true}))
 app.use(express.static('./public'))
 app.get('/', (req, res) => {
-  res.send('<h1>File Upload Starter</h1>')
+  res.send('<h1>Stripe payment Starter</h1>')
 })
-app.use("/api/v1/products", productRouter)
+app.post("/stripe", stripeController)
 
 // app.use('notFoundError')
 // app.use('errorHandlerMiddleware')
