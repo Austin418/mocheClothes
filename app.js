3ae4aa966b2d9a3bd55f1ac54cbd0869bc474f4c
Austin Farwell <austinmfarwell@gmail.com>
2021-12-10 15:30:37 -0700

    merge

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ const stripeController = require("./controllers/stripeController");
 
 const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000
-
 app.use(express.json())
 app.use(fileUpload({useTempFiles: true}))
 app.use(express.static('./public'))
