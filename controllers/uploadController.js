784975528cfb3faf9ce78b57ea7fa2942c9bf17c
Austin Farwell <austinmfarwell@gmail.com>
2021-12-16 13:51:10 -0700

    merge

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,35 +1,35 @@
-const path = require("path");
+const fs = require("fs");
 
-const oldUploadProductImage = async (req, res) => {
-  if (!req.files) {
-    throw newError("no file provided");
-  }
+// const oldUploadProductImage = async (req, res) => {
+//   if (!req.files) {
+//     throw newError("no file provided");
+//   }
 
-  const productImage = req.files.image;
-  if (!productImage.mimetype.startsWith("image")) {
-    throw new Error("choose an img only");
-  }
-  const maxSize = 1024 * 1024;
-  if (productImage.size > maxSize) {
-    throw new Error("the max file size is 1 mb");
-  }
+//   const productImage = req.files.image;
+//   if (!productImage.mimetype.startsWith("image")) {
+//     throw new Error("choose an img only");
+//   }
+//   const maxSize = 1024 * 1024;
+//   if (productImage.size > maxSize) {
+//     throw new Error("the max file size is 1 mb");
+//   }
 
-  const imagePath = path.join(
-    __dirname,
-    "../public/uploads",
-    productImage.name
-  );
-  // console.log(productImage);
-  await productImage.mv(imagePath);
-  res.status(200).json({ image: { src: `/uploads/${productImage.name}` } });
-};
+//   const imagePath = path.join(
+//     __dirname,
+//     "../public/uploads",
+//     productImage.name
+//   );
+//   // console.log(productImage);
+//   await productImage.mv(imagePath);
+//   res.status(200).json({ image: { src: `/uploads/${productImage.name}` } });
+// };
 
 const uploadProductImage = () => {
-  cloudinary.uploader.upload(req.files.image.tempFilepath, {
+  cloudinary.uploader.upload(req.files.image.tempFilePath, {
     use_filename: true,
     folder: "file-upload",
   });
-  refs.unlinkSync(req.fills.image.tempFilePath);
+  fs.unlinkSync(req.files.image.tempFilePath);
   return res.status(200).json({ image: { src: response.secure_url } });
 };
 module.exports = {
--- public/script.js
+++ public/script.js
@@ -3,3 +3,7 @@ console.log(data);
 
 const name = data.name
 
+const nameInput = document.querySelector('#name')
+const priceInput = document.querySelector('#price')
+const imageInput = document.querySelector('#image')
+const fileForm = document.querySelector(".file-form")
